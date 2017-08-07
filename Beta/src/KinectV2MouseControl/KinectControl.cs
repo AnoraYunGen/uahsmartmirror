16e1e379036bc7834e7cc332291d0ce9b62b0bf3
AnoraYunGen <anorayungen@gmail.com>
2017-08-07 01:16:36 -0500

    Add files via upload
    Prevent Duplication of Application instance
    => Basically only one will run at a time
    Added an "Enable" checkbox to enable/disable the debug real time updates in the debug menu
    code improvements

---
+++ Beta/src/KinectV2MouseControl/KinectControl.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+using Microsoft.Kinect;
+using WindowsInput;
+
+namespace KinectV2MouseControl
+{
+    class KinectControl
+    {
+        /// Active Kinect sensor
+        KinectSensor sensor;
+        /// Reader for body frames
+        BodyFrameReader bodyFrameReader;
+        /// Array for the bodies
+        private Body[] bodies = null;
+        /// Screen width and height for determining the exact mouse sensitivity
+        public int screenWidth, screenHeight;
+        /// timer for pause-to-click feature
+        DispatcherTimer timer = new DispatcherTimer();
+        /// How far the cursor move according to your hand's movement
+        public float mouseSensitivity = MOUSE_SENSITIVITY;
+        /// Time required as a pause-clicking
+        public float timeRequired = TIME_REQUIRED;
+        /// The radius range your hand move inside a circle for [timeRequired] seconds would be regarded as a pause-clicking
+        public float pauseThresold = PAUSE_THRESOLD;
+        /// Decide if the user need to do clicks or only move the cursor
+        public bool doClick = DO_CLICK;
+        /// Use Grip gesture to click or not
+        public bool useGripGesture = USE_GRIP_GESTURE;
+        /// Value 0 - 0.95f, the larger it is, the smoother the cursor would move
+        public float cursorSmoothing = CURSOR_SMOOTHING;
+        /// if true the dbg screen would update realtime
+        public bool dbg_md = dbg_md_default;
+        // following values are used to get the values to show in the debug app
+        public float left_x = 0.0f;
+        public float left_y = 0.0f;
+        public float left_z = 0.0f;
+        public float right_x = 0.0f;
+        public float right_y = 0.0f;
+        public float right_z = 0.0f;
+        public float handdistance = 0.0f;
+        public float right_left_x = 0.0f;
+        public float right_left_y = 0.0f;
+        public float right_left_z = 0.0f;
+        public bool magclosed = true;
+        public float spine_x = 0.0f;
+        public float spine_y = 0.0f;
+        public float spine_z = 0.0f;
+        public Int32 cursor_x = 0;
+        public Int32 cursor_y = 0;
+        
+        public string progress = "Connecting to Kinect...";
+        // Default values
+        public const float MOUSE_SENSITIVITY = 3.1f;
+        public const float TIME_REQUIRED = 2f;
+        public const float PAUSE_THRESOLD =70f;
+        public const bool DO_CLICK = true;
+        public const bool USE_GRIP_GESTURE = true;
+        public const float CURSOR_SMOOTHING = 0.95f;
+        public const bool dbg_md_default = false;
+        /// Determine if we have tracked the hand and used it to move the cursor,
+        /// If false, meaning the user may not lift their hands, we don't get the last hand position and some actions like pause-to-click won't be executed.
+        bool alreadyTrackedPos = false;
+        /// for storing the time passed for pause-to-click
+        float timeCount = 0;
+        /// For storing last cursor position
+        Point lastCurPos = new Point(0, 0);
+        /// sleeptime is used for the keypress event
+        private const int sleeptime = 225;
+        /// If true, user did a right hand Grip gesture
+        bool wasLRGrip = false;
+        /// this kinect control is for setting up the program to connect to the connect
+        /// and also setting up the timer for the pause to click
+        public KinectControl()
+        {
+            // get Active Kinect Sensor
+            sensor = KinectSensor.GetDefault();
+            // open the reader for the body frames
+            bodyFrameReader = sensor.BodyFrameSource.OpenReader();
+            bodyFrameReader.FrameArrived += bodyFrameReader_FrameArrived;
+            // get the screen x,y values
+            this.screenHeight = (int)SystemParameters.PrimaryScreenHeight;
+            this.screenWidth = (int)SystemParameters.PrimaryScreenWidth;
+            // set up timer, execute every 0.1s
+            // warning, this timer if ran on the intel compute stick, will cause major lags
+            // use release version!!
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 100); 
+            timer.Tick += new EventHandler(Timer_Tick);
+            timer.Start();
+            // open the sensor
+            sensor.Open();
+        }
+        /// Pause to click timer
+        void Timer_Tick(object sender, EventArgs e)
+        {
+            if (!doClick || useGripGesture)
+                return;
+
+            if (!alreadyTrackedPos)
+            {
+                timeCount = 0;
+                return;
+            }
+            
+            Point curPos = MouseControl.GetCursorPosition();
+
+            if (((lastCurPos - curPos).Length < pauseThresold) && ((timeCount += 0.1f) > timeRequired))
+            {
+                MouseControl.DoMouseClick();
+                timeCount = 0;
+            }
+            else
+                timeCount = 0;
+
+            lastCurPos = curPos;
+        }
+        /// Read body frames
+        void bodyFrameReader_FrameArrived(object sender, BodyFrameArrivedEventArgs e)
+        {
+            bool dataReceived = false;
+            using (BodyFrame bodyFrame = e.FrameReference.AcquireFrame())
+            {
+                if (bodyFrame != null)
+                {
+                    if (this.bodies == null)
+                        this.bodies = new Body[bodyFrame.BodyCount];
+                    // The first time GetAndRefreshBodyData is called, Kinect will allocate each Body in the array.
+                    // As long as those body objects are not disposed and not set to null in the array,
+                    // those body objects will be re-used.
+                    bodyFrame.GetAndRefreshBodyData(this.bodies);
+                    dataReceived = true;
+                }
+            }
+
+            if (!dataReceived) 
+            {
+                alreadyTrackedPos = false;
+                return;
+            }
+            
+            foreach (Body body in this.bodies)
+            {
+                // get first tracked body only, notice there's a break below.
+                if (body.IsTracked)
+                {
+                    // get various skeletal positions
+                    CameraSpacePoint handLeft = body.Joints[JointType.HandLeft].Position;
+                    CameraSpacePoint handRight = body.Joints[JointType.HandRight].Position;
+                    CameraSpacePoint spineBase = body.Joints[JointType.SpineBase].Position;
+                    /* hand x calculated by this. we don't use shoulder right as a reference cause the shoulder right
+                     * is usually behind the lift right hand, and the position would be inferred and unstable.
+                     * because the spine base is on the left of right hand, we plus 0.05f to make it closer to the right.*/
+                    float rx = this.right_x = handRight.X - spineBase.X + 0.05f;
+                    /* hand y calculated by this. ss spine base is way lower than right hand, we plus 0.51f to make it
+                     * higher, the value 0.51f is worked out by testing for a several times, you can set it as another one you like.*/
+                    float ry = this.right_y = spineBase.Y - handRight.Y + 0.51f;
+                    this.right_z = handRight.Z - spineBase.Z;
+                    float lx = this.left_x = handLeft.X - spineBase.X + 0.3f;
+                    float ly = this.left_y = spineBase.Y - handLeft.Y + 0.51f;
+                    this.left_z = spineBase.Z - handLeft.Z;
+
+                    this.spine_x = spineBase.X;
+                    this.spine_y = spineBase.Y;
+                    this.spine_z = spineBase.Z;
+                    this.right_left_x = this.right_x - this.left_x;
+                    this.right_left_y = this.right_y - this.left_y;
+                    this.handdistance = (float)Math.Sqrt(Math.Pow(this.right_left_x, 2) + Math.Pow(this.right_left_y, 2) + Math.Pow(this.right_left_z, 2));
+
+                    this.progress = "hand lift checks";
+                    //if both hands lift up
+                    if ((handRight.Z - spineBase.Z < -0.15f) && (handLeft.Z - spineBase.Z < -0.15f))
+                        dualhands(body);
+                    // if right hand lift forward
+                    else if (handRight.Z - spineBase.Z < -0.15f)
+                        setcursor(rx, ry, body, 1);
+                    // if left hand lift forward
+                    else if (handLeft.Z - spineBase.Z < -0.15f)
+                        setcursor(lx, ly, body, 2);
+                    else
+                    {
+                        this.progress = "Not Tracking any hands";
+                        wasLRGrip = true;
+                        alreadyTrackedPos = false;
+                    }
+
+                    // get first tracked body only
+                    break;
+                }
+            }
+        }
+        /// dual hands
+        private void dualhands(Body body)
+        {
+            this.progress = "dualHands";
+            //this portion (if clause) was added to add another guesture to the original program
+            //specifically the windows magnification tool using keyboard shortcut 'windows'+'+' or 'windows'+'-'
+            alreadyTrackedPos = true;
+            if (this.magclosed == true)
+            {
+                InputSimulator.SimulateModifiedKeyStroke(VirtualKeyCode.LWIN, VirtualKeyCode.ADD);
+                this.magclosed = false;
+            }
+            this.progress = "handchecks";
+            bool righth_closed = body.HandRightState == HandState.Closed;
+            bool lefth_closed = body.HandLeftState == HandState.Closed;
+            
+            this.progress = "to magnify or not to magnify";
+            if (righth_closed && lefth_closed)
+                killmag();
+            else if (righth_closed)
+                InputSimulator.SimulateModifiedKeyStroke(VirtualKeyCode.LWIN, VirtualKeyCode.ADD);
+            else if (lefth_closed)
+                InputSimulator.SimulateModifiedKeyStroke(VirtualKeyCode.RWIN, VirtualKeyCode.SUBTRACT);
+        }
+        /// this function is used to set the cursor relative to the left/right hand position towards the screen
+        private void setcursor (float x, float y, Body body, int hand)
+        {
+            if(hand == 1)
+                    this.progress = "setting cursor with right hand";
+            if(hand == 2)
+                this.progress = "setting cursor with left hand";
+            Point curPos = MouseControl.GetCursorPosition();
+            // smoothing for using should be 0 - 0.95f. The way we smooth the cusor is: oldPos + (newPos - oldPos) * smoothValue
+            float smoothing = 1 - cursorSmoothing;
+            // set cursor position
+            this.cursor_x = (int)(curPos.X + (x * mouseSensitivity * screenWidth - curPos.X) * smoothing);
+            this.cursor_y = (int)(curPos.Y + ((y + 0.25f) * mouseSensitivity * screenHeight - curPos.Y) * smoothing);
+            MouseControl.SetCursorPos(this.cursor_x, this.cursor_y);
+            alreadyTrackedPos = true;
+
+            // Grip gesture
+            /// the following is used to click
+            if (doClick && useGripGesture)
+            {
+                if ((body.HandRightState == HandState.Closed || (body.HandLeftState == HandState.Closed)) && !wasLRGrip)
+                {
+                    this.progress = "clicking";
+                    MouseControl.MouseLeftDown();
+                    wasLRGrip = true;
+                }
+                else if ((body.HandRightState == HandState.Open || body.HandRightState == HandState.Lasso ||
+                          body.HandLeftState == HandState.Open || body.HandLeftState == HandState.Lasso) && wasLRGrip)
+                {
+                    this.progress = "unclicking";
+                    MouseControl.MouseLeftUp();
+                    wasLRGrip = false;
+                }
+            }
+        }
+        /// this killmag funciton is used to kill the magnify application when not using the magnify application
+        public void killmag ()
+        {
+            this.progress = "He's Dead Jim.";
+            System.Diagnostics.Process[] processes = System.Diagnostics.Process.GetProcessesByName("Magnify");
+            if(processes.Length > 0)
+                foreach (System.Diagnostics.Process p in processes)
+                {
+                    try
+                    {
+                        p.Kill();
+                    }
+                    catch//if cannot kill Magnify
+                    {/*do nothing*/}
+                }
+            this.magclosed = true;
+            //sleep for 2 seconds after closing the magnify application
+            System.Threading.Thread.Sleep(2000);
+        }
+        /// this close function is used to stop the timer
+        /// and disconnect from the sensor when the program closes
+        public void Close()
+        {
+            if (timer != null)
+            {
+                timer.Stop();
+                timer = null;
+            }
+            if (this.sensor != null)
+            {
+                this.sensor.Close();
+                this.sensor = null;
+            }
+        }
+    }
+}
---
+++ Beta/src/KinectV2MouseControl/MainWindow.xaml.cs
@@ -0,0 +1,301 @@
+﻿using System.Windows;
+
+namespace KinectV2MouseControl
+{
+    public partial class MainWindow : Window
+    {
+        KinectControl kinectCtrl = new KinectControl();
+
+        System.Windows.Forms.Timer refresh_tmr = new System.Windows.Forms.Timer();
+        const int timer_ms = 750; // for timer refresh
+
+        bool tmr_refresh_enabled = false;
+
+        //is used to start the timer
+        private void tmr_refresh()
+        {
+            refresh_tmr.Tick += new System.EventHandler(refresh_lbls);
+            refresh_tmr.Interval = (1) * (timer_ms);
+            refresh_tmr.Start();
+            tmr_refresh_enabled = true;
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            if(kinectCtrl.dbg_md)
+                tmr_refresh();
+
+            //Checks if the Kinect Control application is still running
+            // if is, then kill second application to prevent duplication application running
+            //this is done to save memory and cpu usage
+            System.Diagnostics.Process[] processes = System.Diagnostics.Process.GetProcessesByName("KinectV2MouseControl");
+            int pcount = processes.Length;
+            if (pcount > 1)
+                foreach (System.Diagnostics.Process p in processes)
+                {
+                    try
+                    {
+                        if(pcount-->1)
+                            p.Kill();
+                    }
+                    catch//if cannot kill Magnify
+                    {/*do nothing*/}
+                }
+            //Hide();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            MouseSensitivity.Value = Properties.Settings.Default.MouseSensitivity;
+            PauseToClickTime.Value = Properties.Settings.Default.PauseToClickTime;
+            PauseThresold.Value = Properties.Settings.Default.PauseThresold;
+            chkEnClick.IsChecked = !Properties.Settings.Default.DoClick;
+            CursorSmoothing.Value = Properties.Settings.Default.CursorSmoothing;
+            chkEndbg.IsChecked = Properties.Settings.Default.EnDbg;
+
+            if (Properties.Settings.Default.GripGesture)
+                rdiGrip.IsChecked = true;
+            else
+                rdiPause.IsChecked = true;
+        }
+
+        private void MouseSensitivity_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (txtMouseSensitivity.IsLoaded)
+            {
+                kinectCtrl.mouseSensitivity = (float)MouseSensitivity.Value;
+                txtMouseSensitivity.Text = kinectCtrl.mouseSensitivity.ToString("f2");
+            }
+        }
+
+        private void PauseToClickTime_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (PauseToClickTime.IsLoaded)
+            {
+                kinectCtrl.timeRequired = (float)PauseToClickTime.Value;
+                txtTimeRequired.Text = kinectCtrl.timeRequired.ToString("f2");
+            }
+        }
+
+        private void PauseThresold_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (PauseThresold.IsLoaded)
+            {
+                kinectCtrl.pauseThresold = (float)PauseThresold.Value;
+                txtPauseThresold.Text = kinectCtrl.pauseThresold.ToString("f2");
+            }
+        }
+
+        private void CursorSmoothing_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (CursorSmoothing.IsLoaded)
+            {
+                kinectCtrl.cursorSmoothing = (float)CursorSmoothing.Value;
+                txtCursorSmoothing.Text = kinectCtrl.cursorSmoothing.ToString("f2");
+            }
+        }
+        //resets the values in the settings tab to the default values set in the program
+        private void btn_Default_Click(object sender, RoutedEventArgs e)
+        {
+            MouseSensitivity.Value = KinectControl.MOUSE_SENSITIVITY;
+            PauseToClickTime.Value = KinectControl.TIME_REQUIRED;
+            PauseThresold.Value = KinectControl.PAUSE_THRESOLD;
+            CursorSmoothing.Value = KinectControl.CURSOR_SMOOTHING;
+
+            chkEnClick.IsChecked = !KinectControl.DO_CLICK;
+            chkEndbg.IsChecked = KinectControl.dbg_md_default;
+            rdiGrip.IsChecked = KinectControl.USE_GRIP_GESTURE;
+            
+            rfsh_lbls();
+        }
+        //this button is used to save the settings in the settings tab
+        private void btn_Save_Click(object sender, RoutedEventArgs e)
+        {
+            Properties.Settings.Default.MouseSensitivity = (float)MouseSensitivity.Value;
+            Properties.Settings.Default.PauseToClickTime = (float)PauseToClickTime.Value;
+            Properties.Settings.Default.PauseThresold = (float)PauseThresold.Value;
+            Properties.Settings.Default.CursorSmoothing = (float)CursorSmoothing.Value;
+            Properties.Settings.Default.PauseThresold = kinectCtrl.pauseThresold;
+            Properties.Settings.Default.GripGesture = kinectCtrl.useGripGesture;
+            Properties.Settings.Default.DoClick = !kinectCtrl.doClick;
+            Properties.Settings.Default.EnDbg = kinectCtrl.dbg_md;
+            Properties.Settings.Default.Save();
+        }
+
+        private void chkEnClick_Checked(object sender, RoutedEventArgs e)
+        {
+            chkEnClickChange();
+        }
+
+        public void chkEnClickChange()
+        {
+            kinectCtrl.doClick = !chkEnClick.IsChecked.Value;
+        }
+
+        private void chkEnClick_Unchecked(object sender, RoutedEventArgs e)
+        {
+            chkEnClickChange();
+        }
+
+        private void chkEndbg_Checked(object sender, RoutedEventArgs e)
+        {
+            chkEndbgChange();
+        }
+
+        public void chkEndbgChange()
+        {
+            if (!tmr_refresh_enabled)
+                tmr_refresh();
+            else
+            {
+                stop_tmr();
+                tmr_refresh_enabled = false;
+            }
+            kinectCtrl.dbg_md = !chkEndbg.IsChecked.Value;
+        }
+
+        public void stop_tmr()
+        {
+            refresh_tmr.Stop();
+            refresh_tmr = null;
+        }
+
+        private void chkEndbg_Unchecked(object sender, RoutedEventArgs e)
+        {
+            chkEndbgChange();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            //stops timer on close
+            stop_tmr();
+            //kills magnify application on close
+            kinectCtrl.killmag();
+            kinectCtrl.Close();
+        }
+
+        public void rdiGripGestureChange()
+        {
+            kinectCtrl.useGripGesture = rdiGrip.IsChecked.Value;
+        }
+
+        private void rdiGrip_Checked(object sender, RoutedEventArgs e)
+        {
+            rdiGripGestureChange();
+        }
+
+        private void rdiPause_Checked(object sender, RoutedEventArgs e)
+        {
+            rdiGripGestureChange();
+        }
+        /// <summary>
+        /// the following functions below in form of btn_XXn_Click or btn_XXp_click is for the negative "n" or positive "p" change in the value
+        /// </summary>
+        //mouse sensitivity
+        private void btn_msn_Click(object sender, RoutedEventArgs e)
+        {
+            MouseSensitivity.Value = kinectCtrl.mouseSensitivity = (float)kinectCtrl.mouseSensitivity - (float)0.1;
+            if (kinectCtrl.mouseSensitivity < 1)
+                MouseSensitivity.Value = kinectCtrl.mouseSensitivity = (float)1;
+            txtMouseSensitivity.Text = kinectCtrl.mouseSensitivity.ToString("f2");
+        }
+
+        private void btn_msp_Click(object sender, RoutedEventArgs e)
+        {
+            MouseSensitivity.Value = kinectCtrl.mouseSensitivity = (float)kinectCtrl.mouseSensitivity + (float)0.1;
+            if (kinectCtrl.mouseSensitivity > 5)
+                MouseSensitivity.Value = kinectCtrl.mouseSensitivity = (float)5;
+            txtMouseSensitivity.Text = kinectCtrl.mouseSensitivity.ToString("f2");
+        }
+        //time required for pause to click
+        private void btn_trn_Click(object sender, RoutedEventArgs e)
+        {
+            PauseToClickTime.Value = kinectCtrl.timeRequired = (float)kinectCtrl.timeRequired - (float)0.1;
+            if (kinectCtrl.timeRequired < 0.3)
+                PauseToClickTime.Value = kinectCtrl.timeRequired = (float)0.3;
+            txtTimeRequired.Text = kinectCtrl.timeRequired.ToString("f2");
+        }
+
+        private void btn_trp_Click(object sender, RoutedEventArgs e)
+        {
+            PauseToClickTime.Value = kinectCtrl.timeRequired = (float)kinectCtrl.timeRequired + (float)0.1;
+            if (kinectCtrl.timeRequired > 5)
+                PauseToClickTime.Value = kinectCtrl.timeRequired = (float)5;
+            txtTimeRequired.Text = kinectCtrl.timeRequired.ToString("f2");
+        }
+        // pause movement threshold
+        private void btn_mtn_Click(object sender, RoutedEventArgs e)
+        {
+            PauseThresold.Value = kinectCtrl.pauseThresold = (float)kinectCtrl.pauseThresold - (float)1;
+            if (kinectCtrl.pauseThresold < 10)
+                PauseThresold.Value = kinectCtrl.pauseThresold = (float)10;
+            txtPauseThresold.Text = kinectCtrl.pauseThresold.ToString("f2");
+        }
+
+        private void btn_mtp_Click(object sender, RoutedEventArgs e)
+        {
+                PauseThresold.Value = kinectCtrl.pauseThresold = (float)kinectCtrl.pauseThresold + (float)1;
+                if (kinectCtrl.pauseThresold > 160)
+                    PauseThresold.Value = kinectCtrl.pauseThresold = (float)160;
+                txtPauseThresold.Text = kinectCtrl.pauseThresold.ToString("f2");
+        }
+        //cursor smoothing
+        private void btn_csn_Click(object sender, RoutedEventArgs e)
+        {
+            CursorSmoothing.Value = kinectCtrl.cursorSmoothing = (float)kinectCtrl.cursorSmoothing - (float)0.05;
+            if (kinectCtrl.cursorSmoothing < 0)
+                CursorSmoothing.Value = kinectCtrl.cursorSmoothing = (float)0;
+            txtCursorSmoothing.Text = kinectCtrl.cursorSmoothing.ToString("f2");
+        }
+
+        private void btn_csp_Click(object sender, RoutedEventArgs e)
+        {
+            CursorSmoothing.Value = kinectCtrl.cursorSmoothing = (float)kinectCtrl.cursorSmoothing + (float)0.05;
+            if (kinectCtrl.cursorSmoothing > 1)
+                CursorSmoothing.Value = kinectCtrl.cursorSmoothing = (float)1;
+            txtCursorSmoothing.Text = kinectCtrl.cursorSmoothing.ToString("f2");
+        }
+        //this function is used to refresh the labels in the program in the settings tab and the debug tab
+        private void refresh_lbls(object sender, System.EventArgs e)
+        {
+            rfsh_lbls();
+        }
+
+        private void rfsh_lbls()
+        {
+            //Main menu value updates
+            txtMouseSensitivity.Text = kinectCtrl.mouseSensitivity.ToString("f2");
+            txtTimeRequired.Text = kinectCtrl.timeRequired.ToString("f2");
+            txtPauseThresold.Text = kinectCtrl.pauseThresold.ToString("f2");
+            txtCursorSmoothing.Text = kinectCtrl.cursorSmoothing.ToString("f2");
+
+            //Debug Menu value updates
+            rix_val.Text = kinectCtrl.right_x.ToString("f2");
+            lex_val.Text = kinectCtrl.left_x.ToString("f2");
+            mox_val.Text = kinectCtrl.cursor_x.ToString("f2");
+            spx_val.Text = kinectCtrl.spine_x.ToString("f2");
+            scx_val.Text = kinectCtrl.screenWidth.ToString("f2");
+            lrx_val.Text = kinectCtrl.right_left_x.ToString("f2");
+            riy_val.Text = kinectCtrl.right_y.ToString("f2");
+            ley_val.Text = kinectCtrl.left_y.ToString("f2");
+            moy_val.Text = kinectCtrl.cursor_y.ToString("f2");
+            spy_val.Text = kinectCtrl.spine_y.ToString("f2");
+            scy_val.Text = kinectCtrl.screenHeight.ToString("f2");
+            lry_val.Text = kinectCtrl.right_left_y.ToString("f2");
+            riz_val.Text = kinectCtrl.right_z.ToString("f2");
+            lez_val.Text = kinectCtrl.left_z.ToString("f2");
+            spz_val.Text = kinectCtrl.spine_z.ToString("f2");
+
+            ha_val.Text = kinectCtrl.handdistance.ToString("f2");
+            se_val.Text = kinectCtrl.mouseSensitivity.ToString("f2");
+            cs_val.Text = kinectCtrl.cursorSmoothing.ToString("f2");
+            kinectCtrl.useGripGesture = rdiGrip.IsChecked.Value;
+            gr_val.Text = kinectCtrl.useGripGesture.ToString();
+            kinectCtrl.doClick = chkEnClick.IsChecked.Value;
+            cl_val.Text = (kinectCtrl.doClick).ToString();
+
+            Program_Progress.Text = "Program Progress: " + kinectCtrl.progress;
+        }
+    }
+}
